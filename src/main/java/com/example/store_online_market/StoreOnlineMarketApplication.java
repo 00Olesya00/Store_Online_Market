bce621a62ee8819304f4c6ce00fbb00298050906
Meshcheryakova <lmavi.kisune@gmail.com>
2023-08-06 13:12:32 +0400

    Initial commit

---
+++ src/main/java/com/example/store_online_market/StoreOnlineMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.store_online_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreOnlineMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreOnlineMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/store_online_market/StoreOnlineMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store_online_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreOnlineMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
